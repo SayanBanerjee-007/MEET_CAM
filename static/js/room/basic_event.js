66312308b99e2925d0394779cc8a233a3dc0c31d
Sayan Banerjee <sayankb.001@gmail.com>
2023-05-07 18:55:25 +0530

    Full screen method add

--- static/js/room/basic_event.js
+++ static/js/room/basic_event.js
@@ -3,11 +3,56 @@ const globalUsers = [];
 const participantNavBtn = document.getElementById("participant-nav-btn");
 const messageNavBtn = document.getElementById("message-nav-btn");
 const messageContent = document.getElementById("message-content");
+const videoSizeButton = document.getElementById("video-size-button");
 const copyUrlBtn = document.getElementById("copy-url-btn");
 const messageText = document.getElementById("message-text");
 
+let hideCursorPointer;
+
 // All Global Functions ---------------------------------------------------------------
+function hideElements() {
+  remoteVideoGrid.classList.add("hide-cursor");
+  videoSizeButton.classList.add("hidden");
+}
+function showElements() {
+  remoteVideoGrid.classList.remove("hide-cursor");
+  videoSizeButton.classList.remove("hidden");
+  clearTimeout(hideCursorPointer);
+  hideCursorPointer = setTimeout(hideElements, 3000);
+}
+
 // All Event Listeners ----------------------------------------------------------------
+document.addEventListener("fullscreenchange", () => {
+  if (videoSizeButton.title === "Enter Full Screen Mode") {
+    videoSizeButton.title = "Exit Full Screen Mode";
+    hideElements();
+    remoteVideoGrid.addEventListener("mousemove", showElements);
+  } else {
+    videoSizeButton.title = "Enter Full Screen Mode";
+    remoteVideoGrid.removeEventListener('mousemove', showElements);
+    clearTimeout(hideCursorPointer);
+    remoteVideoGrid.classList.remove("hide-cursor");
+  }
+});
+videoSizeButton.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    if (remoteVideoGrid.requestFullscreen) {
+      remoteVideoGrid.requestFullscreen();
+    } else if (remoteVideoGrid.webkitRequestFullscreen) {
+      remoteVideoGrid.webkitRequestFullscreen();
+    } else if (remoteVideoGrid.msRequestFullscreen) {
+      remoteVideoGrid.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+});
 participantNavBtn.addEventListener("click", () => {
   messageContent.classList.remove("visible");
   participantNavBtn.firstElementChild.style.color = "#2bff00";
@@ -50,7 +95,7 @@ menuListParticipantMessageBtn.addEventListener("click", () => {
   menuList.classList.remove("visible");
   document.querySelector(".right-side").classList.add("visible");
   document.querySelector(".participant-message").classList.add("visible");
-  localVideoGrid.classList.add('remove-translate');
+  localVideoGrid.classList.add("remove-translate");
   localVideoGrid.classList.add("invisible");
 });
 menuListPresentScreenBtn.addEventListener("click", () => {
@@ -64,6 +109,6 @@ menuListCopyLinkBtn.addEventListener("click", () => {
 rightSideCrossBtn.addEventListener("click", () => {
   document.querySelector(".right-side").classList.remove("visible");
   document.querySelector(".participant-message").classList.remove("visible");
-  localVideoGrid.classList.remove('remove-translate');
+  localVideoGrid.classList.remove("remove-translate");
   localVideoGrid.classList.remove("invisible");
 });
